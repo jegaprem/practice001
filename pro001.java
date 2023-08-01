8b7366e73b743288024db8512ad912b9d56b2361
jegaprem <140790100+jegaprem@users.noreply.github.com>
2023-08-01 13:13:58 +0530

    Update pro001.java

--- pro001.java
+++ pro001.java
@@ -1,1 +1,14 @@
+import java.time.Duration;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class prem {
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.instagram.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.close();
+		}
+         
+}
